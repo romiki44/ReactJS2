2cb3e08ddfbab759ecbda3fdd5c83be87235448e
Roman Kiss <romiki44@hotmail.com>
2019-07-05 13:09:42 +0200

    section05 with classes modules done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from "react";
 //import React, { useState } from "react";
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
-import Radium, { StyleRoot } from 'radium';
 
 class App extends Component {
   state = {
@@ -47,20 +46,8 @@ class App extends Component {
   }
 
   render() {
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': { //Radium
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
-    };
-
     let persons = null;
+    let btnClass='';
 
     if (this.state.showPerson) {
       persons = (
@@ -76,40 +63,36 @@ class App extends Component {
         </div>
       );
 
-      style.backgroundColor = 'red';
-      style[':hover'] = { //Radium
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
+      btnClass=classes.red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     //ak element obsahuje media-query, root element musi byt StyleRoot
     //Person obsahuje dynamicke mediaquery cez Radium
     return (
-      <StyleRoot> 
-        <div className="App">
+
+        <div className={classes.App}>
           <h1>Hi, I'm React!</h1>
-          <p className={classes.join(' ')}>This works :)</p>
+          <p className={assignedClasses.join(' ')}>This works :)</p>
           <button
-            style={style}
+            className={btnClass}
             onClick={this.togglePersonHandler}>
             Toggle Persons
         </button>
           {persons}
         </div>
-      </StyleRoot>
+
     );
   }
   //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello, React!'));
 }
 
-export default Radium(App);
+export default App;
 
